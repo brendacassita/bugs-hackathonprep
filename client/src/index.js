1781df984dcdf4541152d39c669768723c25b9d1
B <brendacassita@Brendas-MacBookPro.local>
2022-03-10 11:44:23 -0700

    bug form

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import "./App.css";
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
